716dd3bdd15fd50564e5c8a6f10cbe67dba9a384
Ralf Heitmann <R.Heitmann@beckhoff.com>
2021-11-25 14:59:53 +0100

    Merged PR 15574: Commit initial version of AdsSecure Sample, implements #62744
    Commit initial version of AdsSecure Sample, implements #62744
    
    Related work items: #62744

---
+++ Sources/AdsSecureSamples/AdsSecureConsoleApp/Program.cs
@@ -0,0 +1,310 @@
+﻿#define TEST
+
+using System;
+using System.Buffers.Binary;
+using System.Net;
+using System.Net.Security;
+using System.Security;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+using TwinCAT.Ads;
+using TwinCAT.Ams;
+using TwinCAT.Tls;
+
+
+namespace SimpleAdsSecureConsoleApp
+{
+    public enum TlsType
+    {
+        SelfCert,
+        CA
+    }
+
+    class Program
+    {
+        /// <summary>
+        /// The local AmsNetId (of the SourceIPC/ClientIPC)
+        /// </summary>
+        static AmsNetId s_localNetId = new AmsNetId("1.1.1.1.1.1");
+
+
+        /// <summary>
+        /// SourceIPC Certificate as string (PEM-Format, copied from .\Certs\SourceIPC.crt)
+        /// </summary>
+        static string s_certificate = @"-----BEGIN CERTIFICATE-----
+MIIDNzCCAh8CFDFkqPhIY5TR1kSDldjD6YgIpBcpMA0GCSqGSIb3DQEBCwUAMFgx
+CzAJBgNVBAYTAkRFMQwwCgYDVQQIDANOUlcxDTALBgNVBAcMBFZlcmwxCzAJBgNV
+BAoMAkJrMQ0wCwYDVQQLDARUQ1BNMRAwDgYDVQQDDAdSQUxGSDA0MB4XDTIxMTEy
+NTExMTQxMFoXDTIyMTEyMDExMTQxMFowWDELMAkGA1UEBhMCREUxDDAKBgNVBAgM
+A05SVzENMAsGA1UEBwwEVmVybDELMAkGA1UECgwCQmsxDTALBgNVBAsMBFRDUE0x
+EDAOBgNVBAMMB1JBTEZIMDQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIB
+AQDgsQQw0dlZxEtqmV27JppGEchMq2hj/o8eFPta08k126AW+miKpwnuZ1tfBp9N
+90+/VcVaEl3KermWpiyTcdLegVodvRqcj+K+qnoYyUE5uPDbDUQhRL8F+3nLbfiZ
+Ml1iwCVpwtY0eEh6lSgDU55zHTZstoQXdUpvkDlouj9GTo+Avip8m14wXKrlx/aQ
+uv9TLBioQJmzC7rXcuWJtBJg5PRzpbkKW9sONazEV7hY69i4igcMtzNIAYk+CZJq
+8x/SLM4RH+14MkagPnk/a7sbFzKaVvLBLLbTZZO/eb7BGXO98xpRv4ZIagEluQpX
+0D8MsQm83Sz6QOqBaAayqWaZAgMBAAEwDQYJKoZIhvcNAQELBQADggEBAMS9SlDv
+vtjKkjfXECj17yev3GkL7b1KAdqy+ISnEYNBHl79SUqXVJu0rzebAcqrZR/UcuTm
+aAhX/qHGB0GOcfq3pEp36QEft82+I92tUjs4bA5OAIdQiyZ58RNi2PULullTNLjq
+nYY4yPh6pSqHwpBgXPK+ZkgRJiZrc3uzrJFzEZNxjHul9Hok2VabjhD7MI1os/rx
+DiF2I+mdl9jVCev5E7qWEg4OOheTz01Hy9tCisK5s3qPFHKcZxftzG8LwFunTSQH
+n/3B+5H3prYbmyOVpq+sMIhVUD0FcsTeJ++9uWNnh26njztG8eEvTEe06en9MLdc
+EU1wxU9DS36L3PQ=
+-----END CERTIFICATE-----";
+
+        /// <summary>
+        /// RootCA certificate (PEM-Format, copied from .\certs\RootCA.pem)
+        /// </summary>
+        static string s_ca = @"-----BEGIN CERTIFICATE-----
+MIIDkTCCAnmgAwIBAgIUVMnOTt6UQ8fsn69ye5W/fgLMk/UwDQYJKoZIhvcNAQEL
+BQAwWDELMAkGA1UEBhMCREUxDDAKBgNVBAgMA05SVzENMAsGA1UEBwwEVmVybDEL
+MAkGA1UECgwCQmsxDTALBgNVBAsMBFRDUE0xEDAOBgNVBAMMB1JBTEZIMDQwHhcN
+MjExMTI1MTExMjA2WhcNMzExMDA0MTExMjA2WjBYMQswCQYDVQQGEwJERTEMMAoG
+A1UECAwDTlJXMQ0wCwYDVQQHDARWZXJsMQswCQYDVQQKDAJCazENMAsGA1UECwwE
+VENQTTEQMA4GA1UEAwwHUkFMRkgwNDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCC
+AQoCggEBAMVVAdKfFYTLgnJWLoU8V40NmmcGEWoA34Y86JcPJSykzlKmCnyTOrQl
+6A8m/JWnJNMp8GBgpBgtXzSQGVW9Nz4c2PiipHXVfvmqAk7DxwfjoyGc3zK3Sq3o
+uXOOLj2rna04ylYSJor7BgIdrWV60lOQXOirO6cd5+dGffYGBTtvbuKADbQvoUPK
+wl64Px8yGgtUB+Y4RWzgidUTXTT508eNflBIAKFvvaju0e1OaDd15EVV90KRD7eU
+J48jxdP3LwO2ha5RBdmZtx8drLCtAW96QUD0pgUKYL9woZoAOh2yjmgaVom4llOd
+V//riUNWrKB490YdYv2dQnXNM3wQYOsCAwEAAaNTMFEwHQYDVR0OBBYEFH52o2q3
+MK9aIxDehGvy6I7ZdtyzMB8GA1UdIwQYMBaAFH52o2q3MK9aIxDehGvy6I7Zdtyz
+MA8GA1UdEwEB/wQFMAMBAf8wDQYJKoZIhvcNAQELBQADggEBAC2AfP3JnhnPpaXI
+dsoPTrRMlkDWHWTe6psQedbzJWkTRyKBktTDnj8cVX6Vvjkiw/IfXgPiJun0cGKv
+kgRk1AYzUs0rGa3Q7jxb6WeN/vcuzgLxJ8Lue/dCs6zSAxtw/TKaZtTpyvY9CurF
+Q4SUUew74x6i0gnMGVTPtOL9cVuWa+eflxwGMYGqyakYjbDK/fPv+fRu/81Wz1+z
+S9I3Y3xrmWE3p184scgOxMzzQdN1c2gyqtf1v3aWgEYK3kVP+IOeDbCXhGTBq1/1
+KU74Jx5x7NqOLE3Y3mCyJzSHrZX6M8zKGhjJhlAYvlpXHByGdpxhIWB+q9NgOsnn
+SeJPHkU=
+-----END CERTIFICATE-----";
+
+        /// <summary>
+        /// RSA Private key as string (PEM format, copied from .\certs\SourceIPC.key)
+        /// </summary>
+        static string s_rsaPrivateKey = @"-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEA4LEEMNHZWcRLaplduyaaRhHITKtoY/6PHhT7WtPJNdugFvpo
+iqcJ7mdbXwafTfdPv1XFWhJdynq5lqYsk3HS3oFaHb0anI/ivqp6GMlBObjw2w1E
+IUS/Bft5y234mTJdYsAlacLWNHhIepUoA1Oecx02bLaEF3VKb5A5aLo/Rk6PgL4q
+fJteMFyq5cf2kLr/UywYqECZswu613LlibQSYOT0c6W5ClvbDjWsxFe4WOvYuIoH
+DLczSAGJPgmSavMf0izOER/teDJGoD55P2u7GxcymlbywSy202WTv3m+wRlzvfMa
+Ub+GSGoBJbkKV9A/DLEJvN0s+kDqgWgGsqlmmQIDAQABAoIBAAPLmamFceltQmTb
+kFS/8y0p0btRzeKagypb6l7/Ys+xFQQuWKO27JkFT+rL/tbfTY6K0VmTI9huuEHO
+LsSS0KAHiVElR5TLbWYRFRzkEWD7Ob5JfBQfyCY+uDDye5eC3Ub0ve0KaHncakT/
+zfw4Zl3FaYzujE6lIYu+4Ole9ACo/PIfudXKk1tJEBLrcgIrs262SYFImrWAWx+0
+NMuRgsYYYYUUI79/Wpgiaxg+Stg4wUtwDUA+RYAcujp6hvn0Kg8/82jmCVoZlg1R
+AcPR1YVW2ZR/hPpgbSEx3zvUuoBsVK0xtqGIbdo9gOnEd7uu8tQn2vm4BwoNLujN
+DiWd8d0CgYEA+Dgk+grdrENmdZThmWyE3MhbMCHXONMpvtCPx28ZGvtmIc59PTcC
+Pq4cWX2Q8w/WnbVfv/I6GDMie2/E9SNRbn55ozQ3HmiVykr0VhmMNExabaCLH8nQ
+ElVAbrVZIHxGzzn6/1LlVBXHxsbl2fhYvBkKN0z7G+s0N0OcmcBN4CcCgYEA57wS
+JTlzbYGP6QF1hxZnQL3B05R+MXFT2VMgDbylEAArbbYeWTidz161Xq+tT+uEWFZW
+Sc9w6JQ9jwetCWVuWdEJvFuIx6EV3Bh0q2XK3bg4R1LOfTHGq/sF03mZ81X5eHSd
+uZR1+SdzepjINETyMJnEKUGhmizUg5s65yAf+z8CgYEAybh29G2YMMKlpbDUlmbG
+otaApOEbkyaoqlW8Qwtaj773BUpWJUVrIZ1FlMSi46VfeNNJeShVZg1IXKA8pCuL
+pgxKtgNdN+0urjOz1bT1aBsU8jqiVbcgzYVS06W1RN06fnZUMOMhU/BPZo+FhFp7
+YoHG96IsAEhpKvBbd+f8YckCgYB4DG8eB6Arh6Yk4FOhUtLUsDkcQd4KARqeCDkf
+xK2CF4RoBqO8Nt9SEU9GKR5Qu8LI/JkhDa0BX+JwGVrj9j7vmqI/iO/X8zRe2/B8
+5nPs3sWQ9W3xX3r7l0RSZLmDXPOrGkanYCiplW12gnWc1mbdFJuRf+WW+EhzkVQ3
+beYDgwKBgGCFgiCRaKLtqYkHcTolNLapPKmfOi4vhNHPSRuHeiK9Qk1zseYL7MGO
+hyybbJB0RgxX7ofiWItbJh9I06ENOBcvjyCReyLoJDoSaqsj0upxfpu2GiDxxRo/
+6+KQclfKaiQvD6kJnVT+zvZ/Gupx/TZcJu3/+uvToItGVvvftjoQ
+-----END RSA PRIVATE KEY-----";
+
+        static async Task Main(string[] args)
+        {
+            bool selfSigned = true;
+
+            CancellationToken cancel = CancellationToken.None;
+
+            bool ok = false;
+            AmsNetId targetNetId;
+
+            do
+            {
+                Console.Write("Enter Target NetId: ");
+                string targetNetIdStr = Console.ReadLine();
+                
+                ok = AmsNetId.TryParse(targetNetIdStr, out targetNetId);
+            } while (!ok);
+
+            int targetPort = 0;
+
+            do
+            {
+                Console.Write("Enter Target AdsPort: ");
+                string targetPortStr = Console.ReadLine();
+                ok = int.TryParse(targetPortStr, out targetPort);
+            } while (!ok);
+
+            
+            AmsAddress targetAddress = new AmsAddress(targetNetId, targetPort);
+
+            string hostOrIp;
+
+            do
+            {
+                Console.Write("Enter Target HostName/IPAddress: ");
+                hostOrIp = Console.ReadLine();
+            } while (string.IsNullOrEmpty(hostOrIp));
+
+
+
+            string? userName = null;
+            SecureString securePwd = new SecureString();
+
+            if (selfSigned)
+            {
+                Console.Write("UserName: ");
+
+                do
+                {
+                    userName = Console.ReadLine();
+                } while (string.IsNullOrEmpty(userName));
+
+                ConsoleKeyInfo key;
+
+                Console.Write("Enter password: ");
+                do
+                {
+                    key = Console.ReadKey(true);
+
+                    // Ignore any key out of range.
+                    //if (((int)key.Key) >= 65 && ((int)key.Key <= 90))
+                    if (((int)key.Key) >= 0x20 && ((int)key.Key <= 0x7E)) // Printable characters
+                    {
+                        // Append the character to the password.
+                        securePwd.AppendChar(key.KeyChar);
+                        Console.Write("*");
+                    }
+                    // Exit if Enter key is pressed.
+                } while (key.Key != ConsoleKey.Enter);
+                Console.WriteLine();
+            }
+
+            X509Certificate2 caCert = X509CertificateHelper.CreateFromPem(s_ca);
+            //CAUTION: The TlsConnect, X509CertificateHelper, AdsReadStateResponseHeader objects are preliminary
+            //and are actually not documented. They could be changed in future!
+
+            // Loading Cert from PEM Formatted strings:
+            X509Certificate2 certFromStrings = X509CertificateHelper.CreateFromPem(s_certificate, s_rsaPrivateKey);
+
+            // Or alternatively (more convenient on Windows)
+            // Get the Certificate (*.pfx) from Cert Store or from File:
+            X509Certificate2 certFromStore;
+            using (X509Store store = new X509Store(StoreLocation.CurrentUser))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                X509Certificate2Collection cers = store.Certificates.Find(X509FindType.FindBySubjectName, "TwinCATTestCertificate", false);
+                if (cers.Count > 0)
+                {
+                    certFromStore = cers[0];
+                };
+            }
+
+            TlsConnect connect = null;
+            try
+            {
+                if (selfSigned)
+                {
+                    connect = new TlsConnectSelfSigned(hostOrIp, certFromStrings, userName, securePwd, s_localNetId);
+                }
+                else
+                {
+                    connect = new TlsConnectCA(hostOrIp, caCert, certFromStrings, s_localNetId);
+                }
+                await connect.ConnectAsync(targetAddress, cancel);
+
+                SslStream sslStream = connect.GetStream();
+
+                string serverHostName = sslStream.TargetHostName;
+                string serverSubject = sslStream.RemoteCertificate.Subject;
+                string serverIssuer = sslStream.RemoteCertificate.Issuer;
+                //string sha1Thumbprint = sslStream.RemoteCertificate.GetCertHashString();
+                string sha256FingerPrint = sslStream.RemoteCertificate.GetSha256Thumbprint();
+
+                Console.WriteLine($"Connected to Server '{serverHostName}' (Subject: {serverSubject}, Issuer: {serverIssuer}");
+
+                bool trustTarget = false;
+
+                if (selfSigned)
+                {
+                    //Ensure that the Server/Target Fingerprint is correct.
+                    //We have to compare the Fingerprint to trust the AdsServer/Target
+                    Console.WriteLine($"Fingerprint: {sha256FingerPrint}");
+                    Console.WriteLine($"Is the Server/Target Fingerprint correct (y/n)?");
+                    ConsoleKeyInfo consoleKey = Console.ReadKey();
+
+                    if (consoleKey.KeyChar == 'y' || consoleKey.KeyChar == 'Y')
+                    {
+                        trustTarget = true;
+                    }
+                }
+                else
+                {
+                    // CA Certification is safe/trusted in itself (both communication ends are certified by the same RootCA)
+                    trustTarget = true;
+                }
+
+                if (trustTarget)
+                {
+                    AdsState state = await ReadStateAsync(connect, CancellationToken.None);
+
+                    Console.WriteLine("");
+                    Console.WriteLine($"SUCCESS: The Ads state of target {targetAddress} is '{state}'.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+            }
+            connect.Close();
+        }
+
+        static async Task<AdsState> ReadStateAsync(TlsConnect connection, CancellationToken cancel)
+        {
+            //CAUTION: The TlsConnect, X509CertificateHelper, AdsReadStateResponseHeader objects are preliminary
+            //and are actually not documented. They could be changed in future!
+
+            AdsState state = AdsState.Invalid;
+            AmsHeader header = new AmsHeader(connection.TargetAddress, connection.LocalAddress, AdsCommandId.ReadState, AmsStateFlags.AdsCommand, 0, 0, 1);
+
+            AmsCommand command = new AmsCommand(header, Array.Empty<byte>());
+            int size = AmsCommandFrameMarshaller.MarshalSize(command);
+
+            byte[] requestBuffer = new byte[size];
+            int marshalledBytes = AmsCommandFrameMarshaller.Marshal(command, requestBuffer.AsSpan());
+
+            SslStream securedStream = connection.GetStream();
+            await securedStream.WriteAsync(requestBuffer, cancel).ConfigureAwait(false);
+
+            int length = AmsHeader.MarshalSize + AdsReadStateResponseHeader.StaticMarshalSize;
+            byte[] responseBuffer = new byte[length];
+            int returnedBytes = await securedStream.ReadAsync(responseBuffer, cancel).ConfigureAwait(false);
+
+            AmsHeader responseHeader;
+            int offset = AmsHeaderMarshaller.Unmarshal(responseBuffer.AsSpan(0), out responseHeader);
+
+            AdsErrorCode adsError = (AdsErrorCode)BinaryPrimitives.ReadUInt32LittleEndian(responseBuffer.AsSpan(offset, 4));
+            offset += 4;
+            state = (AdsState)BinaryPrimitives.ReadUInt16LittleEndian(responseBuffer.AsSpan(offset, 2));
+            offset += 2;
+            ushort deviceState = BinaryPrimitives.ReadUInt16LittleEndian(responseBuffer.AsSpan(offset, 2));
+            return state;
+        }
+    }
+
+    public static class X509CertificateExtension
+    {
+        public static string GetSha256Thumbprint(this X509Certificate cert)
+        {
+            byte[] buffer;
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                buffer = sha256.ComputeHash(cert.GetRawCertData());
+            }
+            string result = BitConverter.ToString(buffer).Replace("-", string.Empty);
+            return result;
+        }
+    }
+}
---
+++ Sources/AdsSecureSamples/AdsSecureConsoleApp/README.md
@@ -0,0 +1,175 @@
+﻿## Description
+Simple AdsSecure Console Application that demonstrates how to establish an ADSSecure channel to a remote TwinCAT System to read the ADSState.
+The Authentication is done via a Self-Signed-Certificate.
+## Requirements
+- *.NET 5.0* or *.NETCORE3.1*
+- *TwinCAT 3.1.4024* on remote system.
+- No other System allocating the same port (e.g. a regular TwinCAT installation) on the local system
+
+*CAUTION:* The **TlsConnect**, **X509CertificateHelper**, **AdsReadStateResponseHeader** objects are preliminary and are actually not documented.
+They could be changed in future!
+
+## Installation
+It is necessary that the local (Sample-running-system) supports at least a TLS Ciphersuite that is used by TwinCAT.
+### Actual TwinCAT supported Ciphersuites
+```
+TLS_DHE_RSA_WITH_AES_256_CBC_SHA256 (0x006b) (Not existing on Windows10?)
+TLS_DHE_RSA_WITH_AES_256_CBC_SHA (0x0039) (Seems to be available)
+TLS_DHE_RSA_WITH_AES_128_CBC_SHA256 (0x0067)
+TLS_DHE_RSA_WITH_AES_128_CBC_SHA (0x0033)
+```
+### Windows Ciphersuites enabled by default
+The List of actually enabled (and supported) ciphersuites can be determined by Powershell:
+```Powershell
+ PS> (get-TlsCiphersuite).Name
+
+TLS_AES_256_GCM_SHA384
+TLS_AES_128_GCM_SHA256
+TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384
+TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256
+TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384
+TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
+TLS_DHE_RSA_WITH_AES_256_GCM_SHA384
+TLS_DHE_RSA_WITH_AES_128_GCM_SHA256
+TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384
+TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256
+TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384
+TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256
+TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA
+TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA
+TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA
+TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA
+TLS_RSA_WITH_AES_256_GCM_SHA384
+TLS_RSA_WITH_AES_128_GCM_SHA256
+TLS_RSA_WITH_AES_256_CBC_SHA256
+TLS_RSA_WITH_AES_128_CBC_SHA256
+TLS_RSA_WITH_AES_256_CBC_SHA
+TLS_RSA_WITH_AES_128_CBC_SHA
+TLS_RSA_WITH_3DES_EDE_CBC_SHA
+TLS_RSA_WITH_NULL_SHA256
+TLS_RSA_WITH_NULL_SHA
+TLS_PSK_WITH_AES_256_GCM_SHA384
+TLS_PSK_WITH_AES_128_GCM_SHA256
+TLS_PSK_WITH_AES_256_CBC_SHA384
+TLS_PSK_WITH_AES_128_CBC_SHA256
+TLS_PSK_WITH_NULL_SHA384
+TLS_PSK_WITH_NULL_SHA256
+```
+
+Here, on a Windows10 System there is no match by default. Trying to use the TlsConnect.Connect method will not open a secure SSL channel.
+
+### Enabling TwinCAT appropriate CipherSuite
+On Windows10 at least the *TLS_DHE_RSA_WITH_AES_256_CBC_SHA* variant is installed, but not enabled by default.
+The CipherSuite can be enabled by the following command:
+
+```Powershell
+Ps> Enable-TlsCipherSuite -Name TLS_DHE_RSA_WITH_AES_256_CBC_SHA
+
+PS> Get-TlsCipherSuite -Name TLS_DHE_RSA_WITH_AES_256_CBC_SHA
+
+KeyType               : 0
+Certificate           : RSA
+MaximumExchangeLength : 1024
+MinimumExchangeLength : 1024
+Exchange              : DH
+HashLength            : 160
+Hash                  : SHA1
+CipherBlockLength     : 16
+CipherLength          : 256
+BaseCipherSuite       : 57
+CipherSuite           : 57
+Cipher                : AES
+Name                  : TLS_DHE_RSA_WITH_AES_256_CBC_SHA
+Protocols             : {769, 770, 771, 65279…}
+```
+
+### Creating a Self-Signed Certificate
+```powershell
+PS> $cert = New-SelfSignedCertificate -DnsName TwinCATTestCertificate -CertStoreLocation cert:\LocalMachine\My
+PS> $cert
+ 
+   PSParentPath: Microsoft.PowerShell.Security\Certificate::LocalMachine\My
+
+Thumbprint                                Subject              EnhancedKeyUsageList
+----------                                -------              --------------------
+9814BEADD027C50B5905DBD769D848D7EE777B78  CN=TwinCATTestCerti… {Client Authentication, Server Authentication}
+```
+### Browsing for Certs
+```powershell
+PS> $cert = Get-ChildItem -Path cert:\LocalMachine\My\ | where Subject -like *TwinCATTestCertificate
+```
+### Test Certificate
+```powershell
+PS> $cert | Test-Certificate -Policy SSL -AllowUntrustedRoot
+```
+### Deleting Certificates
+```powershell
+PS> $cert = Get-ChildItem -Path cert:\LocalMachine\My\ | where Subject -like *TwinCATTestCertificate
+
+PS> $cert | Remove-Item
+```
+### Add TC3 certificate to sample code
+The certificate of the TC3 remote target is stored in TwinCAT\3.1\Target\TcSelfSigned.xml. Add certificate and private key to static string variables s_certificate and s_rsaPrivateKey in Program.cs.
+### Run the sample
+Add remote netId (e.g. about dialog of TwinCAT systray icon), port (e.g. SystemService = 10000, PLC1 = 851), IP,  username/password and compare the fingerprint (e.g. about dialog of TwinCAT systray icon).
+
+### Create certificates for CA AdsSecure
+```powershell
+# Create the ROOT CA Certificate
+mkdir c:\certs
+openssl genrsa -out C:\certs\RootCA.key 2048
+openssl req -x509 -new -nodes -key C:\certs\RootCA.key -sha256 -subj "/C=DE/ST=NRW/L=Verl/O=Bk/OU=TCPM/CN=RootCA" -days 3600 -out C:\certs\RootCA.pem
+
+# Create the Certificate for a IPC
+openssl genrsa -out C:\certs\TargetIPC.key 2048
+openssl req -out C:\certs\TargetIPC.csr -key C:\certs\TargetIPC.key -subj "/C=DE/ST=NRW/L=Verl/O=Bk/OU=TCPM/CN=TargetIPC" –new
+openssl x509 -req -in C:\certs\TargetIPC.csr -CA C:\certs\RootCA.pem -CAkey C:\certs\RootCA.key -CAcreateserial -out C:\certs\TargetIPC.crt -days 360 -sha256
+```
+
+## Using the sample with Certification Authority (CA) certificates
+Using a CA Certificate enforces that the RootCA certificate is known on both sides, the SourceIPC and the TargetIPC.
+The SourceIPC side is hardcoded already in this sample (s_certificate, Program.s_ca, and Program.s_rsaPrivateKey variables).
+The TargetIPC side (if a running TwinCAT system) must have the target-side certificates derived from the RootCA certificate.
+
+Copy the files
+.\Certs\RootCA.pem
+.\Certs\TargetIPC.crt
+.\Certs\TargetIPC.key
+
+to the target system (e.g to the c:\twincat\3.1\target\CACerts\ folder) and
+configure the c:\twincat\3.1\target\StaticRoutes.xml file as follows:
+
+```xml
+<?xml version="1.0"?>
+<TcConfig xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+	<RemoteConnections>
+		<Server>
+			<tls>
+				<ca>c:\twincat\3.1\target\CACerts\RootCA.pem</ca>
+				<cert>c:\twincat\3.1\target\CACerts\TargetIPC.crt</cert>
+				<key>c:\twincat\3.1\target\CACerts\TargetIPC.key</key>
+			</tls>
+		</Server>
+	</RemoteConnections>
+</TcConfig>
+```
+
+Restart or Reconfig the TwinCAT System Service to read the StaticRoutes.xml configuration.
+Run this sample application and enter the Address the of the IPC target system (AmsNetId and Port 10000).
+
+## Using the sample with SelfSigned AdsSecure connection
+For SelfSigning, the access to the target system is authenticated by the Source system certificate plus the Target System credentials.
+Therefore the already included Certificate in the source sample application can be used and no changes are necessary at the target system AdsSecure configuration (StaticRoutes.xml).
+
+On the target IPC side, an empty StaticRoutes.xml should be enough:
+
+```xml
+<?xml version="1.0"?>
+<TcConfig xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+	<RemoteConnections>
+	</RemoteConnections>
+</TcConfig>
+```
+
+
+
