8afd21caab6c52f3046774f66e3d24b5d6a1252c
Chris Knight <ChrisK@beckhoff.com>
2022-03-25 20:59:51 +0000

    adapted the ReadArray sample to seperate the sample into partial class to seperate form creation from sample code, changed sample code to use symbolic access to array, removed memory buffers and used foreach to simplify sample

--- Sources/BaseSamples/ReadArray/Form1.cs
+++ Sources/BaseSamples/ReadArray/Form1.cs
@@ -6,105 +6,38 @@ using System.Windows.Forms;
 using System.Data;
 using TwinCAT.Ads;
 using System.IO;
+using TwinCAT;
+using TwinCAT.TypeSystem;
+using TwinCAT.Ads.TypeSystem;
 
-namespace Sample12
+namespace _12_ReadArray
 {
-	/// <summary>
-	/// Summary description for Form1.
-	/// </summary>
-	public class Form1 : System.Windows.Forms.Form
+	#region CODE_SAMPLE
+	public partial class Form1 : Form
 	{
-		private System.Windows.Forms.Button btnRead;
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.Container components = null;
-		
-		private uint hVar;
-		private System.Windows.Forms.ListBox lbArray;
-		private AdsClient _client;
-	
-		public Form1()
-		{
-			InitializeComponent();		
-		}
-
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		protected override void Dispose( bool disposing )
-		{
-			if( disposing )
-			{
-				if (components != null) 
-				{
-					components.Dispose();
-				}
-			}
-			base.Dispose( disposing );
-		}
-
-		#region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-            this.lbArray = new System.Windows.Forms.ListBox();
-            this.btnRead = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // lbArray
-            // 
-            this.lbArray.ItemHeight = 16;
-            this.lbArray.Location = new System.Drawing.Point(19, 9);
-            this.lbArray.Name = "lbArray";
-            this.lbArray.Size = new System.Drawing.Size(173, 244);
-            this.lbArray.TabIndex = 0;
-            // 
-            // btnRead
-            // 
-            this.btnRead.Location = new System.Drawing.Point(19, 277);
-            this.btnRead.Name = "btnRead";
-            this.btnRead.Size = new System.Drawing.Size(173, 26);
-            this.btnRead.TabIndex = 1;
-            this.btnRead.Text = "Read";
-            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleBaseSize = new System.Drawing.Size(6, 15);
-            this.ClientSize = new System.Drawing.Size(212, 322);
-            this.Controls.Add(this.btnRead);
-            this.Controls.Add(this.lbArray);
-            this.Name = "Form1";
-            this.Text = "Sample12";
-            this.Closing += new System.ComponentModel.CancelEventHandler(this.Form1_Closing);
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-
-		}
-		#endregion
-
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main() 
-		{
-			Application.Run(new Form1());
-		}
-
-#region CODE_SAMPLE
+		AdsClient tcClient = null;
+		ISymbolLoader loader = null;
+		string arrayVar = "";
+		 //Update the AMSNetId for your target
+		AmsNetId targetAmsNetId = AmsNetId.Local;
+		 public Form1()
+        {
+            InitializeComponent();
+        }
 		private void Form1_Load(object sender, System.EventArgs e)
 		{
-			_client = new AdsClient();
-			_client.Connect(851);
+			//Create a new instance of class AdsClient
+			tcClient = new AdsClient();
+
+			//Connect to target PLC - Port 851
+			tcClient.Connect("169.254.139.155.1.1", 851);
 			
 			try
 			{
-				hVar = _client.CreateVariableHandle("MAIN.PLCArray");
+				//Load symbols form target system
+				SymbolLoaderSettings settings = new SymbolLoaderSettings(SymbolsLoadMode.DynamicTree);
+				loader = SymbolLoaderFactory.Create(tcClient, settings);
+				arrayVar = "MAIN.PLCArray";
 			}
 			catch(Exception err)
 			{
@@ -116,19 +49,13 @@ namespace Sample12
 		{
 			try
 			{
-                byte[] readBuffer = new byte[100*2];
+				//Get Array values
+				DynamicSymbol arrayRead = (DynamicSymbol)loader.Symbols[arrayVar]; 
+				short[] readBuffer = (short[])arrayRead.ReadValue();
 
-				//Array komplett auslesen			
-				_client.Read(hVar,readBuffer.AsMemory());
-
-                MemoryStream dataStream = new MemoryStream(readBuffer);
-                BinaryReader binRead = new BinaryReader(dataStream);
-
-                lbArray.Items.Clear();
-				dataStream.Position = 0;			
-				for(int i=0; i<100; i++)
+				foreach (short i in readBuffer)
 				{
-					lbArray.Items.Add(binRead.ReadInt16().ToString());
+					lbArray.Items.Add(i.ToString());
 				}
 			}
 			catch(Exception err)
@@ -137,19 +64,25 @@ namespace Sample12
 			}
 		}
 
-		private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
 		{
-			//Resourcen wieder freigeben
-			try
+			if( disposing )
 			{
-				_client.DeleteVariableHandle(hVar);					
+				// Dispose the Client during Form Cleanup
+				tcClient.Dispose();
+
+				if (components != null) 
+				{
+					components.Dispose();
+				}
 			}
-			catch(Exception err)
-			{
-				MessageBox.Show(err.Message);
-			}
-			_client.Dispose();				
+			base.Dispose( disposing );
 		}
         #endregion
-    }
+	}
+	
+    
 }
