798687e89b919613ff90e0bc611399f3a684ab98
Chris Knight <ChrisK@beckhoff.com>
2022-04-01 16:47:06 +0100

    modified sample to use the SumCommand namespace allowing use of SumSymbolRead and SumSymbolWrite with symbolic access, reduces demo complexity

--- Sources/BaseSamples/SumReadWrite/Form1.cs
+++ Sources/BaseSamples/SumReadWrite/Form1.cs
@@ -8,26 +8,16 @@ using System.ComponentModel;
 using System.Windows.Forms;
 using System.Data;
 using System.IO;
+using TwinCAT;
 using TwinCAT.Ads;
+using TwinCAT.Ads.TypeSystem;
+using TwinCAT.Ads.SumCommand;
+using TwinCAT.TypeSystem;
+using TwinCAT.ValueAccess;
+
 
 namespace _22_SumReadWrite
 {
-    // Structure declaration for values
-    internal struct MyStruct
-    {
-        public ushort uintValue;
-        public int dintValue;
-        public bool boolValue;
-    }
-
-    // Structure declaration for handles
-    internal struct VariableInfo
-    {
-        public uint indexGroup;
-        public uint indexOffset;
-        public int length;
-    }
-
     /// <summary>
     /// Summary description for Form1.
     /// </summary>
@@ -35,9 +25,9 @@ namespace _22_SumReadWrite
     {
         
         private AdsClient adsClient;
-        private string[] variableNames;
-        private int[] variableLengths;
-        VariableInfo[] variables;
+        private ISymbolLoader loader;
+        SymbolCollection symbols = new SymbolCollection();
+        private string[] variablePaths;
 
         public Form1()
         {
@@ -74,10 +64,6 @@ namespace _22_SumReadWrite
             base.Dispose(disposing);
         }
 
-        
-
-        
-
         private void Form1_Load(object sender, System.EventArgs e)
         {
             try
@@ -87,16 +73,14 @@ namespace _22_SumReadWrite
                 adsClient.Connect("5.95.143.126.1.1",851);
 
                 // Fill structures with name and size of PLC variables
-                variableNames = new string[] { "MAIN.uintValue", "MAIN.dintValue", "MAIN.boolValue" };
-                variableLengths = new int[] { 2, 4, 1 };
+                variablePaths = new string[] { "MAIN.uintValue", "MAIN.dintValue", "MAIN.boolValue" };
 
-                // Write handle parameter into structure
-                variables = new VariableInfo[variableNames.Length];
-                for (int i = 0; i < variables.Length; i++)
+                SymbolLoaderSettings settings = new SymbolLoaderSettings(SymbolsLoadMode.DynamicTree);
+                loader = SymbolLoaderFactory.Create(adsClient, settings);
+
+                foreach (string str in variablePaths)
                 {
-                    variables[i].indexGroup = (int)AdsReservedIndexGroup.SymbolValueByHandle;
-                    variables[i].indexOffset = adsClient.CreateVariableHandle(variableNames[i]);
-                    variables[i].length = variableLengths[i];
+                    symbols.Add((DynamicSymbol)loader.Symbols[str]);
                 }
             }
             catch (Exception err)
@@ -115,60 +99,18 @@ namespace _22_SumReadWrite
 
             try
             {
-                // Get the ADS return codes and examine for errors
-                byte[] readBuffer = BlockRead(variables);
+                SumSymbolRead readCommand = new SumSymbolRead(adsClient, symbols);
+                
+                var resultSumRead = readCommand.Read();
 
-                BinaryReader reader = new BinaryReader(new MemoryStream(readBuffer));
-                for (int i = 0; i < variables.Length; i++)
-                {
-                    int error = reader.ReadInt32();
-                    if (error != (int)AdsErrorCode.NoError)
-                        System.Diagnostics.Debug.WriteLine(
-                            String.Format("Unable to read variable {0} (Error = {1})", i, error));
-                }
-
-                // Read the data from the ADS stream
-                MyStruct myStruct;
-                myStruct.uintValue = reader.ReadUInt16();
-                myStruct.dintValue = reader.ReadInt32();
-                myStruct.boolValue = reader.ReadBoolean();
-
-                // Write data from the structure into the text boxes
-                tbUint.Text = myStruct.uintValue.ToString();
-                tbDint.Text = myStruct.dintValue.ToString();
-                tbBool.Text = myStruct.boolValue.ToString();
+                tbUint.Text = resultSumRead[0].ToString();
+                tbDint.Text = resultSumRead[1].ToString();
+                tbBool.Text = resultSumRead[2].ToString();
             }
             catch (Exception err)
             {
                 MessageBox.Show(err.Message);
             }
-        }
-
-        private byte[] BlockRead(VariableInfo[] variables)
-        {
-            // Allocate memory
-            int rdLength = variables.Length * 4;
-            int wrLength = variables.Length * 12;
-
-            // Write data for handles into the ADS Stream
-            byte[] writeBuffer = new byte[wrLength];
-
-            BinaryWriter writer = new BinaryWriter(new MemoryStream(writeBuffer));
-            for (int i = 0; i < variables.Length; i++)
-            {
-                writer.Write(variables[i].indexGroup);
-                writer.Write(variables[i].indexOffset);
-                writer.Write(variables[i].length);
-                rdLength += variables[i].length;
-            }
-
-            // Sum command to read variables from the PLC
-            //AdsStream rdStream = new AdsStream(rdLength);
-            byte[] readBuffer = new byte[rdLength];
-            adsClient.ReadWrite((uint)0xF080, (uint)variables.Length, readBuffer.AsMemory(), writeBuffer.AsMemory() );
-
-            // Return the ADS error codes
-            return readBuffer;
         }
 
         // Write variables into the PLC
@@ -180,58 +122,18 @@ namespace _22_SumReadWrite
 
             try
             {
-                // Get the ADS return codes and examine for errors
-                byte[] readBuffer = BlockRead2(variables);
+                SumSymbolWrite writeCommand = new SumSymbolWrite(adsClient,symbols);
 
-                BinaryReader reader = new BinaryReader(new MemoryStream(readBuffer));
-                for (int i = 0; i < variables.Length; i++)
-                {
-                    int error = reader.ReadInt32();
-                    if (error != (int)AdsErrorCode.NoError)
-                        System.Diagnostics.Debug.WriteLine(
-                            String.Format("Unable to read variable {0} (Error = {1})", i, error));
-                }
+                object[] writeValues = new object[]{Convert.ToInt16(tbUint2.Text),
+                                                    Convert.ToInt16(tbDint2.Text),
+                                                    Convert.ToBoolean(tbBool2.Text) };
+                
+                writeCommand.Write(writeValues);
             }
             catch (Exception err)
             {
                 MessageBox.Show(err.Message);
             }
-        }
-
-        private byte[] BlockRead2(VariableInfo[] variables)
-        {
-            // Allocate memory
-            int rdLength = variables.Length * 4;
-            int wrLength = variables.Length * 12 + 7;
-
-            byte[] writeBuffer = new byte[wrLength];
-
-            BinaryWriter writer = new BinaryWriter(new MemoryStream(writeBuffer));
-            MyStruct myStruct;
-            myStruct.uintValue = ushort.Parse(tbUint2.Text);
-            myStruct.dintValue = int.Parse(tbDint2.Text);
-            myStruct.boolValue = bool.Parse(tbBool2.Text);
-            
-            // Write data for handles into the ADS stream
-            for (int i = 0; i < variables.Length; i++)
-            {
-                writer.Write(variables[i].indexGroup);
-                writer.Write(variables[i].indexOffset);
-                writer.Write(variables[i].length);
-            }
-
-            // Write data to send to PLC behind the structure
-            writer.Write(myStruct.uintValue);
-            writer.Write(myStruct.dintValue);
-            writer.Write(myStruct.boolValue);
-
-            // Sum command to write the data into the PLC
-            byte[] readBuffer = new byte[rdLength];
-
-            adsClient.ReadWrite((uint)0xF081, (uint)variables.Length, readBuffer.AsMemory(), writeBuffer.AsMemory());
-
-            // Return the ADS error codes
-            return readBuffer;
         }
     }
 }
