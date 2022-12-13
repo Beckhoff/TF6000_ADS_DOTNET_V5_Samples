c0f4b409ba2de9240947a11e21cce4dc8fe19d05
Ralf Heitmann <r.heitmann@beckhoff.de>
2022-12-13 18:29:49 +0100

    Adds sample for Broadcast search (implementation of Powershell host)

---
+++ Sources/ManagementSamples/PwshBroadcastSearch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Management.Automation;
+
+
+// TcXaeMgmt must be installed from powershell Gallery
+string name = "TcXaeMgmt";
+
+// Alternatively the TcXaeMgmt Module can be loaded from an XCopy Deployed folder
+// string path = c:\tmp\TcXaeMgmt\6.0.68\TcXaeMgmt.psd1
+PowerShell sh = PowerShell.Create();
+
+//Import Module
+sh.AddStatement()
+    .AddCommand("Import-Module")
+    .AddParameter("Name", name).Invoke();
+
+//Broadcast Search
+Console.WriteLine("Starting BroadCast Search ...");
+
+var result = sh.AddStatement()
+   .AddCommand("Get-AdsRoute")
+   .AddParameter("all", true)
+   .Invoke();
+
+foreach(var r in result)
+{
+    Console.WriteLine(r.BaseObject.ToString());
+}
+
+Console.WriteLine("");
+Console.WriteLine("Completed");
+Console.WriteLine("");
+Console.WriteLine("Press Enter");
+Console.ReadLine();
